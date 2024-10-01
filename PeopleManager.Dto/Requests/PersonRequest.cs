b27ecf55926e8d9e22ade937e157e1e02a198410
IanWanzeele <ian.wanzeele@travelnote.com>
2024-10-01 16:03:18 +0200

    Added person request and organizationd dto's

---
+++ PeopleManager.Dto/Requests/PersonRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleManager.Dto.Requests
+{
+    public class PersonRequest
+    {
+        [Display(Name = "First Name")]
+        [Required]
+        public required string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required]
+
+        public required string LastName { get; set; }
+
+        public string? Email { get; set; }
+
+        public int? OrganizationId { get; set; }
+    }
+
+}
+
---
+++ PeopleManager.Dto/Results/PersonResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleManager.Dto.Results
+{
+    public class PersonResult
+    {
+        
+        public int Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string? Email { get; set; }
+
+        public int? OrganizationId { get; set; }
+        public string? OrganizationName { get; set; }
+    }
+}
+
